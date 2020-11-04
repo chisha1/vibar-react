af4f88af17cb8e776af3752d1b20e0c4307a4c0b
Chishamiso Chivhenge <chishachivhenge@yahoo.co.uk>
2020-11-04 09:59:59 +0000

    Installed axios and JSON Server, REST service for artist data, Data status for loading, errors and successs

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Link from 'next/link';
 import styled from 'styled-components'
 import Grid from '@material-ui/core/Grid';
@@ -38,72 +39,9 @@ const ArtistContainer = styled.div`
 //#endregion
 
 const Artists = () => {
-    const artistsArray = [
-        {
-            name: 'Travis Scott',
-            id: 1,
-            following: false,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Kanye West',
-            id: 2,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Drake',
-            id: 3,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Ariana Grande',
-            id: 4,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Taylor Swift',
-            id: 5,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Post Malone',
-            id: 6,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'DaBaby',
-            id: 7,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Eminem',
-            id: 8,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-        {
-            name: 'Billie Eilish',
-            id: 9,
-            following: true,
-            bio:
-                'Artist bio',
-        },
-    ];
+    
 
+    //#region Functions
     function toggleArtistFollow(artistRec) {
         return {
             ...artistRec,
@@ -111,46 +49,90 @@ const Artists = () => {
         }
     }
 
-    function followingToggleHandler(artistRec) {
+    async function followingToggleHandler(artistRec) {
         const toggledArtistRec = toggleArtistFollow(artistRec); //store following toggled artist
         const artistIndex = artists.map((artist) => artist.id).indexOf(artistRec.id); //get index of artist from artists list
-        setArtists //create new arra of artists, setting the state
-            ([...artists.slice(0, artistIndex), toggledArtistRec, ...artists.slice(artistIndex + 1)]);
+        try {
+            await axios.put(`http://localhost:4000/artists/$${artistRec.id}`, toggledArtistRec)
+            setArtists //create new arra of artists, setting the state
+                ([...artists.slice(0, artistIndex), toggledArtistRec, ...artists.slice(artistIndex + 1)]);
+        } catch (e) {
+            setStatus(REQUEST_STATUS.ERROR);
+            setError(e);
+        }
     }
+    //#endregion
 
     const classes = useStyles();
     const [searchQuery, setSearchQuery] = useState('');
-    const [artists, setArtists] = useState(artistsArray);
+    const [artists, setArtists] = useState([]);
+
+    const REQUEST_STATUS = {
+        LOADING: "loading",
+        SUCCESS: "success",
+        ERROR: "error"
+    }
+
+    const [status, setStatus] = useState(REQUEST_STATUS.LOADING);
+    const [error, setError] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:4000/artists");
+                setArtists(response.data);
+                setStatus(REQUEST_STATUS.SUCCESS);
+            } catch (e) {
+                setStatus(REQUEST_STATUS.ERROR);
+                setError(e);
+            }
+        }
+        fetchData();
+    }, [])
+
+    const success = status === REQUEST_STATUS.SUCCESS;
+    const isLoading = status === REQUEST_STATUS.LOADING;
+    const hasErrored = status === REQUEST_STATUS.ERROR;
 
     return (
         <div>
             <ArtistSearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
             <div className={classes.root} style={{ marginTop: '10px' }}>
-                <Grid container spacing={3}
-                    direction="row"
-                    justify="space-evenly"
-                    alignItems="center"
-                >
+                {isLoading && <div>Loading...</div>}
+                {hasErrored && (
+                    <div>
+                        Loading error...
+                        <br />
+                        <b>ERROR: {error.message}</b>
+                    </div>
+                )}
+                {success && (
+                    <Grid container spacing={3}
+                        direction="row"
+                        justify="space-evenly"
+                        alignItems="center"
+                    >
 
-                    {artists
-                        .filter((artist) => {
-                            const targetString = `${artist.name}`.toLowerCase();
-                            return searchQuery.length === 0
-                                ? true
-                                : targetString.includes(searchQuery.toLowerCase());
-                        })
-                        .map((artist) => (
-                        <Grid item xs={6} sm={3} key={artist.id}>
-                            <ArtistContainer>
-                                    <FollowIndicator
-                                        className={ artist.following && 'active' }
-                                        onClick={() => followingToggleHandler(artist)}
-                                    />
-                                    <ArtistImage {...artist} width={'100%'} />
-                            </ArtistContainer>
-                        </Grid>
-                    ))}
-                </Grid>
+                        {artists
+                            .filter((artist) => {
+                                const targetString = `${artist.name}`.toLowerCase();
+                                return searchQuery.length === 0
+                                    ? true
+                                    : targetString.includes(searchQuery.toLowerCase());
+                            })
+                            .map((artist) => (
+                                <Grid item xs={6} sm={3} key={artist.id}>
+                                    <ArtistContainer>
+                                        <FollowIndicator
+                                            className={artist.following && 'active'}
+                                            onClick={() => followingToggleHandler(artist)}
+                                        />
+                                        <ArtistImage {...artist} width={'100%'} />
+                                    </ArtistContainer>
+                                </Grid>
+                            ))}
+                    </Grid>
+                )}
             </div>
         </div>
     );
